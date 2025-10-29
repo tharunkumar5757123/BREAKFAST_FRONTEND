686660feab4c0cf12f3ec1153754fa11f0c0279b
tharunkumar5757123 <tharun5757123@gmail.com>
2025-10-30 01:09:21 +0530

    Fix CartContext import case for Vercel build

--- fast/src/main.jsx
+++ fast/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./context/cartContext.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
